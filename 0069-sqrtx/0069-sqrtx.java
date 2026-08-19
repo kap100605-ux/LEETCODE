543e49e4ea399f3590869df03431abb81175a9d0
Kanika Panwar <kap100605@gmail.com>
2026-08-19 20:18:53 +0530

    Time: 55 ms (5.31%), Space: 42.9 MB (8.62%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        
+        
+        for (int i = 1; i <= x; i++) {
+            
+            if (i == x / i) {
+                return i;
+            }
+            
+            if (i > x / i) {
+                return i - 1;
+            }
+        }
+        return 1;
+    }
+}
